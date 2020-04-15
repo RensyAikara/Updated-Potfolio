43501dd6819414a6391c5f97108f84133b4ad9f3
Rensy <rensy.aikara@gmail.com>
2020-04-14 21:04:21 -0500

    added index.html portfolio.html newportfolio.js and contact.html files

---
+++ Assets/Images/Document_files/newportfolio.js
@@ -0,0 +1,20 @@
+var image = document.getElementById("firstProject");
+image.setAttribute("src","./Assets/Images/weatherEats-04.png");
+
+var linkTo = document.getElementById("link");
+linkTo.setAttribute("href","https://cbruder1292.github.io/Project1/");
+linkTo.setAttribute("target","_blank");
+
+var linkImage = document.getElementById("linkedin");
+linkImage.setAttribute("src","./Assets/Images/linkedin.png");
+
+var linkedinProfile = document.getElementById("linkedinProfileLink");
+linkedinProfile.setAttribute("href","https://linkedin.com/in/rensy-a-894463198/");
+linkedinProfile.setAttribute("target","_blank");
+
+var githubImage = document.getElementById("githubimage");
+githubImage.setAttribute("src","./Assets/Images/githubImage.png");
+
+var githubProfile = document.getElementById("githubLink");
+githubProfile.setAttribute("href","https://github.com/RensyAikara?tab=repositories");
+githubProfile.setAttribute("target","_blank");
